e0a21c53578f1352ed6c45ad47bb03ab04599cc1
Ph4ntom <b4rcode@pm.me>
2020-11-05 20:07:49 +0100

    Delete datasource properties

--- src/main/java/dao/database/DBService.java
+++ src/main/java/dao/database/DBService.java
@@ -13,19 +13,11 @@ public class DBService {
     private DBService() {
         Config conf = ConfigFactory.datasource();
         dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.MariaDbDataSource");
         dataSource.setJdbcUrl(conf.getValue("jdbcUrl"));
         dataSource.setUsername(conf.getValue("username"));
         dataSource.setPassword(conf.getValue("password"));
-        dataSource.addDataSourceProperty("cachePrepStmts", "true");
-        dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
-        dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         dataSource.addDataSourceProperty("useServerPrepStmts", "true");
-        dataSource.addDataSourceProperty("useLocalSessionState", "true");
-        dataSource.addDataSourceProperty("rewriteBatchedStatements", "true");
-        dataSource.addDataSourceProperty("cacheResultSetMetadata", "true");
-        dataSource.addDataSourceProperty("cacheServerConfiguration", "true");
-        dataSource.addDataSourceProperty("elideSetAutoCommits", "true");
-        dataSource.addDataSourceProperty("maintainTimeStats", "false");
     }
 
     public static HikariDataSource getInstance() {
