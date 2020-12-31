9cb0f591044d53f271457835c30e78b261701cf7
Ph4ntom <b4rcode@pm.me>
2020-12-31 01:18:26 +0100

    Delete the loop
    - The user name is a unique value into the database, a loop is not useful anymore
    
    - Remove blacklist guild check

--- src/main/java/listeners/member/MemberName.java
+++ src/main/java/listeners/member/MemberName.java
@@ -1,37 +1,31 @@
 package listeners.member;
 
-import java.util.List;
-
-import configuration.cache.Blacklist;
 import dao.database.Dao;
-import dao.pojo.MemberPojo;
+import dao.pojo.PMember;
 import factory.DaoFactory;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.user.update.UserUpdateNameEvent;
-import net.dv8tion.jda.api.exceptions.ContextException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import proxy.ProxyUtils;
 
-/**
- * This class doesn't trigger when MemberCachePolicy is set to 'NONE'.
- */
 public class MemberName extends ListenerAdapter {
 
+    /**
+     * Indicates that the username of a User changed. (Not Nickname).<br />
+     * Note: The old member has to be cached to compare the old and new usernames.<br />
+     * Scenarios: <br />
+     * 
+     * - If the ChunkingFilter is set to NONE and the MemberCachePolicy is set to ALL, this method will not trigger the first time a user update his name, because
+     * the user is not cached, It will be after an action from him (username update, nickname update, message sent, etc).
+     * 
+     * - If the ChunkingFilter is set to NONE and the MemberCachePolicy is set to NONE, this method will never trigger at all.
+     */
     @Override
     public void onUserUpdateName(UserUpdateNameEvent event) {
         if (!event.getUser().isBot()) {
-            List<Guild> guilds = event.getJDA().getGuilds();
-
-            for (int i = 0; i < guilds.size(); i++) {
-                if (!(boolean) Blacklist.getInstance().getUnchecked(guilds.get(i).getId())) {
-                    guilds.get(i).retrieveMemberById(event.getUser().getId(), false).queue(user -> {
-                        MemberPojo member = ProxyUtils.getMemberFromCache(user);
-                        member.setName(event.getNewName());
-                        Dao<MemberPojo> memberDao = DaoFactory.getMemberDAO();
-                        memberDao.update(member);
-                    }, ContextException.here(acceptor -> {
-                    }));
-                }
+            Dao<PMember> memberDao = DaoFactory.getMemberDAO();
+            PMember member = memberDao.find(event.getUser().getId());
+            if (!member.isEmpty()) {
+                member.setName(event.getNewName());
+                memberDao.update(member);
             }
         }
     }
