62e9a696b46147f202a196c732a24cd61e134b5f
Ph4ntom <b4rcode@pm.me>
2020-12-31 01:26:10 +0100

    Use dataSourceClassName instead of jdbcUrl
    Convert dataSource singleton as an enum singleton

--- src/main/java/dao/database/DBService.java
+++ src/main/java/dao/database/DBService.java
@@ -1,33 +1,33 @@
 package dao.database;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import configuration.files.Config;
+import configuration.file.Config;
 import factory.ConfigFactory;
 
-public class DBService {
+public enum DBService {
 
-    private static volatile DBService instance;
-    private static HikariDataSource dataSource;
+    INSTANCE;
+
+    private final HikariDataSource dataSource;
 
     private DBService() {
-        Config conf = ConfigFactory.datasource();
-        dataSource = new HikariDataSource();
-        dataSource.setDriverClassName("org.mariadb.jdbc.MariaDbDataSource");
-        dataSource.setJdbcUrl(conf.getValue("jdbcUrl"));
-        dataSource.setUsername(conf.getValue("username"));
-        dataSource.setPassword(conf.getValue("password"));
-        dataSource.addDataSourceProperty("useServerPrepStmts", "true");
+        Config conf = ConfigFactory.getDatasource();
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(conf.getValue("className"));
+        config.setUsername(conf.getValue("username"));
+        config.setPassword(conf.getValue("password"));
+        config.addDataSourceProperty("databaseName", conf.getValue("database"));
+        config.addDataSourceProperty("portNumber", conf.getValue("port"));
+        config.addDataSourceProperty("serverName", conf.getValue("server"));
+        config.addDataSourceProperty("ssl", conf.getValue("ssl"));
+        config.addDataSourceProperty("sslmode", conf.getValue("sslmode"));
+        config.addDataSourceProperty("sslrootcert", conf.getValue("ca"));
+        dataSource = new HikariDataSource(config);
     }
 
-    public static HikariDataSource getInstance() {
-        if (instance == null) {
-            synchronized (DBService.class) {
-                if (instance == null) {
-                    instance = new DBService();
-                }
-            }
-        }
+    public HikariDataSource getDatasource() {
         return dataSource;
     }
 
