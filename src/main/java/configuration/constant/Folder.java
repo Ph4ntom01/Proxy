f0765694452a5c7e7083a7b59882175afcb636e1
Ph4ntom <b4rcode@pm.me>
2020-12-31 14:46:04 +0100

    Delete config folder

--- src/main/java/configuration/constant/Folder.java
+++ src/main/java/configuration/constant/Folder.java
@@ -2,7 +2,7 @@ package configuration.constant;
 
 public enum Folder {
 
-    RESOURCES("resources/"), CONFIG("config/"), LOG("logs/"), IMG("img/"), ISSOU("img/issou/");
+    RESOURCES("resources/"), LOG("logs/"), IMG("img/"), ISSOU("img/issou/");
 
     private final String name;
 
