49bf1b775c6dd5a5b236e498df46e45f88a8bfed
Ph4ntom <b4rcode@pm.me>
2020-12-31 01:14:59 +0100

    Delete the member if he exists into the database
    - Rename entities
    
    - Remove blacklist guild check

--- src/main/java/listeners/member/MemberLeave.java
+++ src/main/java/listeners/member/MemberLeave.java
@@ -2,55 +2,57 @@ package listeners.member;
 
 import java.time.LocalDate;
 
-import configuration.cache.Blacklist;
 import dao.database.Dao;
-import dao.pojo.LeaveChannelPojo;
-import dao.pojo.GuildPojo;
-import dao.pojo.MemberPojo;
+import dao.pojo.PGuild;
+import dao.pojo.PGuildMember;
+import dao.pojo.PLeaveChannel;
 import factory.DaoFactory;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import proxy.ProxyEmbed;
-import proxy.ProxyUtils;
+import proxy.utility.ProxyCache;
+import proxy.utility.ProxyEmbed;
+import proxy.utility.ProxyString;
 
 public class MemberLeave extends ListenerAdapter {
 
-    private GuildPojo guild;
+    private PGuild guild;
 
     @Override
     public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
-        if (!(boolean) Blacklist.getInstance().getUnchecked(event.getGuild().getId()) && !event.getUser().isBot()) {
-            guild = ProxyUtils.getGuildFromCache(event.getGuild());
+        if (!event.getUser().isBot()) {
+            guild = ProxyCache.getGuild(event.getGuild());
             LocalDate shield = LocalDate.now().minusDays(guild.getShield());
             LocalDate userTimeCreated = event.getUser().getTimeCreated().toLocalDate();
 
             if (!userTimeCreated.isAfter(shield)) {
-                removeMemberDatas(event);
+                removeMemberData(event);
                 sendLeaveMessage(event);
             }
         }
     }
 
-    private void removeMemberDatas(GuildMemberRemoveEvent event) {
-        Dao<MemberPojo> memberDao = DaoFactory.getMemberDAO();
-        MemberPojo member = memberDao.find(event.getGuild().getId() + "#" + event.getUser().getId());
-        memberDao.delete(member);
+    private void removeMemberData(GuildMemberRemoveEvent event) {
+        Dao<PGuildMember> gMemberDao = DaoFactory.getGuildMemberDAO();
+        PGuildMember gMember = gMemberDao.find(event.getGuild().getId() + "#" + event.getUser().getId());
+        if (!gMember.isEmpty()) {
+            gMemberDao.delete(gMember);
+        }
     }
 
     private void sendLeaveMessage(GuildMemberRemoveEvent event) {
         if (guild.getLeaveChannel() != null) {
-            Dao<LeaveChannelPojo> leaveChannelDao = DaoFactory.getLeaveChannelDAO();
-            LeaveChannelPojo leaveChannel = leaveChannelDao.find(guild.getLeaveChannel());
+            Dao<PLeaveChannel> leaveChannelDao = DaoFactory.getLeaveChannelDAO();
+            PLeaveChannel leaveChannel = leaveChannelDao.find(guild.getLeaveChannel());
             try {
                 if (leaveChannel.getMessage() != null && !leaveChannel.getEmbed()) {
-                    event.getGuild().getTextChannelById(guild.getLeaveChannel()).sendMessage(ProxyUtils.getMemberMessageEvent(leaveChannel.getMessage(), event.getUser())).queue();
+                    event.getGuild().getTextChannelById(guild.getLeaveChannel()).sendMessage(ProxyString.getMemberMessageEvent(leaveChannel.getMessage(), event.getUser())).queue();
 
                 } else if (leaveChannel.getMessage() != null && leaveChannel.getEmbed()) {
                     ProxyEmbed embed = new ProxyEmbed();
                     embed.controlGateEvent(event.getUser());
                     event.getGuild().getTextChannelById(guild.getLeaveChannel()).sendMessage(embed.getEmbed().build())
-                            .append(ProxyUtils.getMemberMessageEvent(leaveChannel.getMessage(), event.getUser())).queue();
+                            .append(ProxyString.getMemberMessageEvent(leaveChannel.getMessage(), event.getUser())).queue();
 
                 } else if (leaveChannel.getMessage() == null && leaveChannel.getEmbed()) {
                     ProxyEmbed embed = new ProxyEmbed();
