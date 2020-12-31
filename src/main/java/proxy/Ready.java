7fa197b7bad3ef58e22389ba40e229d3be6eecf0
Ph4ntom <b4rcode@pm.me>
2020-12-31 01:23:29 +0100

    No more load the entire members list on a ReadyEvent
    - The members are now created dynamically into the database, if I load the members at startup, the main thread may disconnect JDA from discord
    
    - Rename entities
    
    - Remove blacklist guild check
    
    - Add getGuildsFromDiscord method

--- src/main/java/proxy/Ready.java
+++ src/main/java/proxy/Ready.java
@@ -1,14 +1,15 @@
 package proxy;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import configuration.cache.Blacklist;
 import dao.database.Dao;
 import dao.database.GuildDAO;
-import dao.pojo.GuildPojo;
-import dao.pojo.MemberPojo;
+import dao.pojo.PGuild;
 import factory.DaoFactory;
 import factory.PojoFactory;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -25,43 +26,45 @@ public class Ready extends ListenerAdapter {
     }
 
     private void guildsLeave(Event event) {
-        Dao<GuildPojo> guildDao = DaoFactory.getGuildDAO();
-        Set<GuildPojo> discordGuilds = ProxyUtils.getGuildsFromDiscord(event.getJDA().getGuilds());
-        Set<GuildPojo> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
+        Dao<PGuild> guildDao = DaoFactory.getGuildDAO();
+        Set<PGuild> discordGuilds = getGuildsFromDiscord(event.getJDA().getGuilds());
+        Set<PGuild> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
         databaseGuilds.removeAll(discordGuilds);
 
         if (!databaseGuilds.isEmpty()) {
-            GuildPojo guild = PojoFactory.getGuild();
-            for (GuildPojo guildTmp : databaseGuilds) {
+            for (PGuild guildTmp : databaseGuilds) {
+                PGuild guild = PojoFactory.getGuild();
                 guild.setId(guildTmp.getId());
-                guild.setMembers(((GuildDAO) guildDao).findMembers(guildTmp.getId()));
-                ((GuildDAO) guildDao).delete(guild.getMembers());
                 guildDao.delete(guild);
             }
         }
     }
 
     private void guildsJoin(Event event) {
-        Dao<GuildPojo> guildDao = DaoFactory.getGuildDAO();
-        Set<GuildPojo> discordGuilds = ProxyUtils.getGuildsFromDiscord(event.getJDA().getGuilds());
-        Set<GuildPojo> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
+        Dao<PGuild> guildDao = DaoFactory.getGuildDAO();
+        Set<PGuild> discordGuilds = getGuildsFromDiscord(event.getJDA().getGuilds());
+        Set<PGuild> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
         discordGuilds.removeAll(databaseGuilds);
 
         if (!discordGuilds.isEmpty()) {
-            GuildPojo guild = PojoFactory.getGuild();
-            for (GuildPojo guildTmp : discordGuilds) {
+            for (PGuild guildTmp : discordGuilds) {
+                PGuild guild = PojoFactory.getGuild();
                 guild.setId(guildTmp.getId());
                 guild.setName(guildTmp.getName());
                 guildDao.create(guild);
-
-                if (!(boolean) Blacklist.getInstance().getUnchecked(guild.getId())) {
-                    event.getJDA().getGuildById(guild.getId()).loadMembers().onSuccess(members -> ((GuildDAO) guildDao).create(ProxyUtils.getMembersFromDiscord(members)));
-                } else {
-                    Dao<MemberPojo> memberDao = DaoFactory.getMemberDAO();
-                    event.getJDA().getGuildById(guild.getId()).retrieveOwner().queue(owner -> memberDao.create(ProxyUtils.getGuildOwnerFromDiscord(owner)));
-                }
             }
         }
+    }
+
+    private Set<PGuild> getGuildsFromDiscord(List<Guild> guilds) {
+        Set<PGuild> discordGuilds = new HashSet<>();
+        for (Guild guildTmp : guilds) {
+            PGuild guild = PojoFactory.getGuild();
+            guild.setId(guildTmp.getId());
+            guild.setName(guildTmp.getName());
+            discordGuilds.add(guild);
+        }
+        return discordGuilds;
     }
 
 }
