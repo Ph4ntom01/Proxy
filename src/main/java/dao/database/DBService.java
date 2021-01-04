fd0067b1e29b22e2c1d822ad88222c3a1d16820e
Ph4ntom <b4rcode@pm.me>
2021-01-04 12:03:42 +0100

    Add datasource properties

--- src/main/java/dao/database/DBService.java
+++ src/main/java/dao/database/DBService.java
@@ -18,12 +18,15 @@ public enum DBService {
         config.setDataSourceClassName(conf.getString("database.className"));
         config.setUsername(conf.getString("database.username"));
         config.setPassword(conf.getString("database.password"));
+        config.addDataSourceProperty("ApplicationName", conf.getString("database.applicationName"));
         config.addDataSourceProperty("databaseName", conf.getString("database.databaseName"));
         config.addDataSourceProperty("portNumber", conf.getLong("database.port"));
         config.addDataSourceProperty("serverName", conf.getString("database.server"));
         config.addDataSourceProperty("ssl", conf.getBoolean("database.ssl"));
         config.addDataSourceProperty("sslmode", conf.getString("database.sslMode"));
         config.addDataSourceProperty("sslrootcert", conf.getString("database.ca"));
+        config.addDataSourceProperty("prepareThreshold", conf.getLong("database.prepareThreshold"));
+        config.addDataSourceProperty("preparedStatementCacheSizeMiB", conf.getLong("database.preparedStatementCacheSizeMiB"));
         dataSource = new HikariDataSource(config);
     }
 
