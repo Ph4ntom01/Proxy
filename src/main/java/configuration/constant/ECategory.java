60e246e0d75e113bef74ead42130b2f9900a4a8c
Ph4ntom <b4rcode@pm.me>
2021-02-10 00:21:27 +0100

    Rename Category to ECategory

--- src/main/java/configuration/constant/ECategory.java
+++ src/main/java/configuration/constant/ECategory.java
@@ -1,12 +1,12 @@
 package configuration.constant;
 
-public enum Category {
+public enum ECategory {
 
     ADMINISTRATION("administration"), MODERATION("moderation"), UTILITY("utility"), MEME("meme");
 
     private final String name;
 
-    private Category(String name) {
+    private ECategory(String name) {
         this.name = name;
     }
 
