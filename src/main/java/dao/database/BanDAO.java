4069eb7fd98c2a02a813371a194a21bd261abf45
Ph4ntom <b4rcode@pm.me>
2021-02-10 00:58:46 +0100

    Add BanDAO class

---
+++ src/main/java/dao/database/BanDAO.java
@@ -0,0 +1,70 @@
+package dao.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import dao.pojo.PBan;
+
+public class BanDAO extends ADao<PBan> {
+
+    protected BanDAO(HikariDataSource datasource) {
+        super(datasource);
+    }
+
+    @Override
+    public boolean create(PBan bMember) {
+        String query = "INSERT INTO ban(guild_id, member_id, ban_date) VALUES(?, ?, ?);";
+        try (Connection conn = datasource.getConnection(); PreparedStatement pst = conn.prepareStatement(query);) {
+            pst.setLong(1, bMember.getGuildId());
+            pst.setLong(2, bMember.getId());
+            pst.setTimestamp(3, bMember.getBanDate());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean delete(PBan bMember) {
+        String query = "DELETE FROM ban WHERE guild_id = ? AND member_id = ?;";
+        try (Connection conn = datasource.getConnection(); PreparedStatement pst = conn.prepareStatement(query);) {
+            pst.setLong(1, bMember.getGuildId());
+            pst.setLong(2, bMember.getId());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean update(PBan bMember) {
+        return false;
+    }
+
+    @Override
+    public PBan find(Long... bMemberLong) {
+        PBan bMember = null;
+        String query = "SELECT guild_id, member_id, ban_date FROM ban WHERE guild_id = ? AND member_id = ?;";
+        try (Connection conn = datasource.getConnection(); PreparedStatement pst = conn.prepareStatement(query);) {
+            pst.setLong(1, bMemberLong[0]);
+            pst.setLong(2, bMemberLong[1]);
+            ResultSet rs = pst.executeQuery();
+            rs.next();
+            bMember = new PBan();
+            if (rs.getRow() != 0) {
+                bMember.setGuildId(rs.getLong("guild_id"));
+                bMember.setId(rs.getLong("member_id"));
+                bMember.setBanDate(rs.getTimestamp("ban_date"));
+            }
+        } catch (SQLException e) {
+        }
+        return bMember;
+    }
+
+}
