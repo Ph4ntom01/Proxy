c20c7a24cf5a0499dbb72afd3316f32d80b6deb2
Ph4ntom <b4rcode@pm.me>
2021-02-09 23:59:14 +0100

    Recreate class and move to commands.guildowner new package

---
+++ src/main/java/commands/guildowner/SetPermission.java
@@ -0,0 +1,80 @@
+package commands.guildowner;
+
+import commands.ACommand;
+import configuration.cache.EGuildMemberCache;
+import configuration.constant.ECommand;
+import configuration.constant.EPermission;
+import dao.database.ADao;
+import dao.database.DaoFactory;
+import dao.pojo.PGuild;
+import dao.pojo.PGuildMember;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.ContextException;
+import net.dv8tion.jda.api.requests.RestAction;
+
+public class SetPermission extends ACommand {
+
+    private EPermission permission;
+
+    public SetPermission(GuildMessageReceivedEvent event, String[] args, ECommand command, PGuild guild, EPermission permission) {
+        super(event, args, command, guild);
+        this.permission = permission;
+    }
+
+    public SetPermission(GuildMessageReceivedEvent event, ECommand command, PGuild guild) {
+        super(event, command, guild);
+    }
+
+    @Override
+    public void execute() {
+        if (getAuthor().getId().equals(getGuild().getOwnerId())) {
+            RestAction<Member> command = retrieveMentionnedMember(1, false);
+            if (command == null) { return; }
+            command.queue(mentionnedMember -> {
+                if (mentionnedMember.getUser().isBot()) {
+                    sendMessage("Impossible to set a permission for a bot.");
+                } else if (mentionnedMember.isOwner()) {
+                    sendMessage("Impossible to set a permission for yourself.");
+                } else {
+                    String userTag = mentionnedMember.getUser().getAsTag();
+                    EGuildMemberCache.INSTANCE.getGuildMemberAsync(mentionnedMember).thenAcceptAsync(member -> {
+                        if (permission == member.getPermission()) {
+                            sendMessage("**" + userTag + "** already has this permission.");
+                        } else {
+                            member.setPermission(permission);
+                            ADao<PGuildMember> gMemberDao = DaoFactory.getGuildMemberDAO();
+                            gMemberDao.update(member);
+                            sendMessage("**" + userTag + "**" + " is now " + "**" + permission.getName().toLowerCase() + "**" + ".");
+                        }
+                    });
+                }
+            }, ContextException.here(acceptor -> sendMessage("**" + getArgs()[1] + "** is not a member.")));
+        } else {
+            sendMessage("Only the guild owner has the ability to set a permission.");
+        }
+    }
+
+    @Override
+    public void help(boolean embedState) {
+        if (embedState) {
+            // @formatter:off
+            sendHelpEmbed(
+                    "Set a permission for a mentionned member.\n\n- "
+                    + EPermission.USER.getName() + ": `" + getGuildPrefix() + ECommand.SETUSER.getName() + "`\n\n- "
+                    + EPermission.MODERATOR.getName() + ": `" + getGuildPrefix() + ECommand.SETMODO.getName() + "`\n\n- "
+                    + EPermission.ADMINISTRATOR.getName() + ": `" + getGuildPrefix() + ECommand.SETADMIN.getName() + "`\n\n"
+                    + "Example: `" + getGuildPrefix() + ECommand.SETADMIN.getName() + " @aMember`.\n\n"
+                    + "*You can also mention a member by his ID*.\n"
+                    + "**This command can only be used by the server owner**.");
+            // @formatter:on
+        } else {
+            // @formatter:off
+            sendMessage("Set a permission for a mentionned member. "
+                    + "**Example:** `" + getGuildPrefix() + getCommandName() + " @aMember`.\n"
+                    + "**This command can only be used by the guild owner**.");
+            // @formatter:on
+        }
+    }
+
+}
