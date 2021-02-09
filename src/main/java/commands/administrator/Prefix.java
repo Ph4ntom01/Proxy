72016c2eb5fb3946a4086d789284720ed8909ee5
Ph4ntom <b4rcode@pm.me>
2021-02-10 00:11:39 +0100

    Rename class SetPrefix to Prefix

---
+++ src/main/java/commands/administrator/Prefix.java
@@ -0,0 +1,45 @@
+package commands.administrator;
+
+import commands.ACommand;
+import configuration.constant.ECommand;
+import dao.database.ADao;
+import dao.database.DaoFactory;
+import dao.pojo.PGuild;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class Prefix extends ACommand {
+
+    public Prefix(GuildMessageReceivedEvent event, String[] args, ECommand command, PGuild guild) {
+        super(event, args, command, guild);
+    }
+
+    public Prefix(GuildMessageReceivedEvent event, ECommand command, PGuild guild) {
+        super(event, command, guild);
+    }
+
+    @Override
+    public void execute() {
+        if (getArgs()[1].length() <= 2) {
+            if (getGuildPrefix().equalsIgnoreCase(getArgs()[1])) {
+                sendMessage("Prefix " + "**" + getGuildPrefix() + "**" + " has already been defined.");
+            } else {
+                ADao<PGuild> guildDao = DaoFactory.getGuildDAO();
+                getPGuild().setPrefix(getArgs()[1]);
+                guildDao.update(getPGuild());
+                sendMessage("Prefix is now: " + "**" + getGuildPrefix() + "**");
+            }
+        } else {
+            sendMessage("Two characters maximum are allowed.");
+        }
+    }
+
+    @Override
+    public void help(boolean embedState) {
+        if (embedState) {
+            sendHelpEmbed("Change the prefix of the server.\n\nExample: `" + getGuildPrefix() + getCommandName() + " $`");
+        } else {
+            sendMessage("Change the prefix of the server. **Example:** `" + getGuildPrefix() + getCommandName() + " $`.");
+        }
+    }
+
+}
