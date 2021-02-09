fccc5778a5ff177338944c265807dcf153cfde5c
Ph4ntom <b4rcode@pm.me>
2021-02-10 00:50:32 +0100

    Add invite command
    Bot owner command

---
+++ src/main/java/commands/botowner/Invite.java
@@ -0,0 +1,73 @@
+package commands.botowner;
+
+import java.awt.Color;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
+
+import commands.ACommand;
+import configuration.constant.ECommand;
+import dao.database.ADao;
+import dao.database.DaoFactory;
+import dao.database.LogInviteDAO;
+import dao.pojo.PGuild;
+import dao.pojo.PLogInvite;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.ParsingException;
+import net.dv8tion.jda.api.utils.data.DataObject;
+
+public class Invite extends ACommand {
+
+    public Invite(GuildMessageReceivedEvent event, ECommand command, PGuild guild) {
+        super(event, command, guild);
+    }
+
+    @Override
+    public void execute() {
+        getMessage().delete().queue();
+        ADao<PLogInvite> logDao = DaoFactory.getLogInviteDAO();
+        Set<PLogInvite> logs = ((LogInviteDAO) logDao).findLogs(true);
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.ORANGE);
+        embed.setTitle(":bar_chart: Logs");
+        embed.addField("", ":shinto_shrine: __**Invite**__", false);
+        for (PLogInvite logTmp : logs) {
+            DataObject json = DataObject.fromJson(logTmp.getGuildLog());
+            // @formatter:off
+            embed.addField("> _" + json.getString("name") + "_", "```ini"
+                    + "\n[ID]:    " + json.getLong("id")
+                    + "\n[Date]:  " + Timestamp.valueOf(json.getString("date")).toLocalDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) + "```", false);
+            // @formatter:on
+        }
+        logs = ((LogInviteDAO) logDao).findLogs(false);
+        embed.addField("", ":kaaba: __**Leave**__", false);
+        for (PLogInvite logTmp : logs) {
+            DataObject json = DataObject.fromJson(logTmp.getGuildLog());
+            // @formatter:off
+            embed.addField("> _" + json.getString("name") + "_", "```ini"
+                    + "\n[ID]:               " + json.getLong("id")
+                    + "\n[Join Channel]:     " + checkNJSON(json, "join_channel_id")
+                    + "\n[Leave Channel]:    " + checkNJSON(json, "leave_channel_id")
+                    + "\n[Control Channel]:  " + checkNJSON(json, "control_channel_id")
+                    + "\n[Default Role]:     " + checkNJSON(json, "default_role_id")
+                    + "\n[Prefix]:           " + json.getString("prefix")
+                    + "\n[Shield]:           " + json.getInt("shield") 
+                    + "\n[Date]:             " + Timestamp.valueOf(json.getString("date")).toLocalDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) + "```", false);
+            // @formatter:on
+        }
+        sendPrivateEmbedToBotOwner(embed);
+    }
+
+    private Object checkNJSON(DataObject json, String key) {
+        try {
+            return json.get(key);
+        } catch (ParsingException e) {
+            return "None";
+        }
+    }
+
+    @Override
+    public void help(boolean embedState) {}
+
+}
