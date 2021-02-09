f24a8bd3d75fe4ffad828c773e175656f98e7c6a
Ph4ntom <b4rcode@pm.me>
2021-02-10 00:59:24 +0100

    Move DaoFactory class to dao.database package

---
+++ src/main/java/dao/database/DaoFactory.java
@@ -0,0 +1,47 @@
+package dao.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import dao.pojo.PBan;
+import dao.pojo.PGuild;
+import dao.pojo.PGuildMember;
+import dao.pojo.PJoinChannel;
+import dao.pojo.PLeaveChannel;
+import dao.pojo.PLogInvite;
+import dao.pojo.PMember;
+
+public class DaoFactory {
+
+    private static HikariDataSource datasource = EDBService.INSTANCE.getDatasource();
+
+    private DaoFactory() {}
+
+    public static ADao<PGuild> getGuildDAO() {
+        return new GuildDAO(datasource);
+    }
+
+    public static ADao<PJoinChannel> getJoinChannelDAO() {
+        return new JoinChannelDAO(datasource);
+    }
+
+    public static ADao<PLeaveChannel> getLeaveChannelDAO() {
+        return new LeaveChannelDAO(datasource);
+    }
+
+    public static ADao<PGuildMember> getGuildMemberDAO() {
+        return new GuildMemberDAO(datasource);
+    }
+
+    public static ADao<PMember> getMemberDAO() {
+        return new MemberDAO(datasource);
+    }
+
+    public static ADao<PBan> getBanDAO() {
+        return new BanDAO(datasource);
+    }
+
+    public static ADao<PLogInvite> getLogInviteDAO() {
+        return new LogInviteDAO(datasource);
+    }
+
+}
