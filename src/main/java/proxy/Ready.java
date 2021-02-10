5da90ee0eea490dfe5ef1a2bb128487514ce2945
Ph4ntom <b4rcode@pm.me>
2021-02-10 01:45:01 +0100

    Replace pojo factory by directly instantiate the object
    - Rename Dao to ADao
    
    - Keep method declaration code on one line if empty

--- src/main/java/proxy/Ready.java
+++ src/main/java/proxy/Ready.java
@@ -4,11 +4,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import dao.database.Dao;
+import dao.database.ADao;
+import dao.database.DaoFactory;
 import dao.database.GuildDAO;
 import dao.pojo.PGuild;
-import factory.DaoFactory;
-import factory.PojoFactory;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.ReadyEvent;
@@ -16,8 +15,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Ready extends ListenerAdapter {
 
-    protected Ready() {
-    }
+    protected Ready() {}
 
     @Override
     public void onReady(ReadyEvent event) {
@@ -26,14 +24,14 @@ public class Ready extends ListenerAdapter {
     }
 
     private void guildsLeave(Event event) {
-        Dao<PGuild> guildDao = DaoFactory.getGuildDAO();
+        ADao<PGuild> guildDao = DaoFactory.getGuildDAO();
         Set<PGuild> discordGuilds = getGuildsFromDiscord(event.getJDA().getGuilds());
         Set<PGuild> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
         databaseGuilds.removeAll(discordGuilds);
 
         if (!databaseGuilds.isEmpty()) {
             for (PGuild guildTmp : databaseGuilds) {
-                PGuild guild = PojoFactory.getGuild();
+                PGuild guild = new PGuild();
                 guild.setId(guildTmp.getId());
                 guildDao.delete(guild);
             }
@@ -41,14 +39,14 @@ public class Ready extends ListenerAdapter {
     }
 
     private void guildsJoin(Event event) {
-        Dao<PGuild> guildDao = DaoFactory.getGuildDAO();
+        ADao<PGuild> guildDao = DaoFactory.getGuildDAO();
         Set<PGuild> discordGuilds = getGuildsFromDiscord(event.getJDA().getGuilds());
         Set<PGuild> databaseGuilds = ((GuildDAO) guildDao).findGuilds();
         discordGuilds.removeAll(databaseGuilds);
 
         if (!discordGuilds.isEmpty()) {
             for (PGuild guildTmp : discordGuilds) {
-                PGuild guild = PojoFactory.getGuild();
+                PGuild guild = new PGuild();
                 guild.setId(guildTmp.getId());
                 guild.setName(guildTmp.getName());
                 guildDao.create(guild);
@@ -59,8 +57,8 @@ public class Ready extends ListenerAdapter {
     private Set<PGuild> getGuildsFromDiscord(List<Guild> guilds) {
         Set<PGuild> discordGuilds = new HashSet<>();
         for (Guild guildTmp : guilds) {
-            PGuild guild = PojoFactory.getGuild();
-            guild.setId(guildTmp.getId());
+            PGuild guild = new PGuild();
+            guild.setId(guildTmp.getIdLong());
             guild.setName(guildTmp.getName());
             discordGuilds.add(guild);
         }
