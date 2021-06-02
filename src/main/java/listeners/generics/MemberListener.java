1d93adbe44bbfd488d60b684ca0dae792e8097c2
Ph4ntom <b4rcode@pm.me>
2021-06-02 23:42:52 +0200

    Move GuildMemberJoinRemoveModel class to listeners.generics.MemberModel file
    Logging first implementation

--- src/main/java/listeners/generics/MemberListener.java
+++ src/main/java/listeners/generics/MemberListener.java
@@ -1,37 +1,17 @@
-package listeners;
+package listeners.generics;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.font.TextAttribute;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.sql.Timestamp;
-import java.text.AttributedString;
 import java.time.LocalDate;
 import java.util.Objects;
-
-import javax.imageio.ImageIO;
+import java.util.logging.Logger;
 
 import configuration.cache.EGuildCache;
-import configuration.constant.EFolder;
 import dao.database.ADao;
 import dao.database.DaoFactory;
 import dao.pojo.PBan;
-import dao.pojo.PGuild;
 import dao.pojo.PGuildMember;
-import dao.pojo.PJoinChannel;
-import dao.pojo.PLeaveChannel;
 import dao.pojo.PMember;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildBanEvent;
 import net.dv8tion.jda.api.events.guild.GuildUnbanEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
@@ -46,6 +26,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MemberListener extends ListenerAdapter {
 
+    private static final Logger LOG = Logger.getLogger(MemberListener.class.getName());
+
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         if (!event.getUser().isBot()) {
@@ -55,13 +37,14 @@ public class MemberListener extends ListenerAdapter {
                 if (userTimeCreated.isAfter(shield)) {
                     try {
                         event.getMember().kick().queue();
-                    } catch (InsufficientPermissionException e) {
-                    } catch (HierarchyException e) {
+                    } catch (InsufficientPermissionException | HierarchyException e) {
+                        LOG.log(java.util.logging.Level.WARNING, e.getMessage());
                     }
                 } else {
-                    sendWelcomeMessage(event, guild);
+                    MemberModel member = new MemberModel(event, guild);
+                    member.sendWelcomeMessage();
                     if (guild.getControlChannel() == null) {
-                        addDefRole(event.getGuild(), guild, event.getUser());
+                        member.addDefRole(event.getGuild(), event.getUser());
                     }
                 }
             });
@@ -80,7 +63,8 @@ public class MemberListener extends ListenerAdapter {
                     if (!gMember.isEmpty()) {
                         gMemberDao.delete(gMember);
                     }
-                    sendLeaveMessage(event, guild);
+                    MemberModel member = new MemberModel(event, guild);
+                    member.sendLeaveMessage();
                 }
             });
         }
@@ -110,7 +94,8 @@ public class MemberListener extends ListenerAdapter {
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
         EGuildCache.INSTANCE.getGuildAsync(event.getGuild().getIdLong()).thenAcceptAsync(guild -> {
             if (Objects.equals(event.getChannel().getIdLong(), guild.getControlChannel()) && event.getReactionEmote().getAsCodepoints().equalsIgnoreCase("U+2705")) {
-                addDefRole(event.getGuild(), guild, event.getUser());
+                MemberModel member = new MemberModel();
+                member.addDefRole(event.getGuild(), event.getUser());
             }
         });
     }
@@ -122,9 +107,8 @@ public class MemberListener extends ListenerAdapter {
                 try {
                     Role defaultRole = event.getGuild().getRoleById(guild.getDefaultRole());
                     event.getGuild().removeRoleFromMember(event.getUserId(), defaultRole).queue();
-                } catch (NullPointerException e) {
-                } catch (HierarchyException e) {
-                } catch (ErrorResponseException e) {
+                } catch (NullPointerException | HierarchyException | ErrorResponseException e) {
+                    LOG.log(java.util.logging.Level.WARNING, e.getMessage());
                 }
             }
         });
@@ -155,168 +139,6 @@ public class MemberListener extends ListenerAdapter {
                 memberDao.update(member);
             }
         }
-    }
-
-    private void addDefRole(Guild guildJda, PGuild guild, User user) {
-        if (guild.getDefaultRole() != null) {
-            try {
-                Role defaultRole = guildJda.getRoleById(guild.getDefaultRole());
-                guildJda.addRoleToMember(user.getId(), defaultRole).queue();
-            } catch (HierarchyException e) {
-            } catch (ErrorResponseException e) {
-            }
-        }
-    }
-
-    private void sendWelcomeMessage(GuildMemberJoinEvent event, PGuild guild) {
-        if (guild.getJoinChannel() != null) {
-            ADao<PJoinChannel> joinChannelDao = DaoFactory.getJoinChannelDAO();
-            PJoinChannel joinChannel = joinChannelDao.find(guild.getJoinChannel());
-            try {
-                if (joinChannel.getMessage() != null && !joinChannel.getEmbed()) {
-                    event.getGuild().getTextChannelById(joinChannel.getChannelId()).sendMessage(joinChannel.getMessage().replace("[member]", event.getUser().getName())).queue();
-
-                } else if (joinChannel.getMessage() != null && joinChannel.getEmbed()) {
-                    GuildMemberJoinRemoveModel join = new GuildMemberJoinRemoveModel();
-                    join.sendJoinMessage(event, "join_channel_full", joinChannel);
-
-                } else if (joinChannel.getMessage() == null && joinChannel.getEmbed()) {
-                    GuildMemberJoinRemoveModel join = new GuildMemberJoinRemoveModel();
-                    join.sendJoinMessage(event, "join_channel_only_box", joinChannel);
-                }
-            } catch (InsufficientPermissionException e) {
-            }
-        }
-    }
-
-    private void sendLeaveMessage(GuildMemberRemoveEvent event, PGuild guild) {
-        if (guild.getLeaveChannel() != null) {
-            ADao<PLeaveChannel> leaveChannelDao = DaoFactory.getLeaveChannelDAO();
-            PLeaveChannel leaveChannel = leaveChannelDao.find(guild.getLeaveChannel());
-            try {
-                if (leaveChannel.getMessage() != null && !leaveChannel.getEmbed()) {
-                    event.getGuild().getTextChannelById(leaveChannel.getChannelId()).sendMessage(leaveChannel.getMessage().replace("[member]", event.getUser().getName())).queue();
-                }
-
-                else if (leaveChannel.getMessage() != null && leaveChannel.getEmbed()) {
-                    GuildMemberJoinRemoveModel leave = new GuildMemberJoinRemoveModel();
-                    leave.sendLeaveMessage(event, "leave_channel_full", leaveChannel);
-                }
-
-                else if (leaveChannel.getMessage() == null && leaveChannel.getEmbed()) {
-                    GuildMemberJoinRemoveModel leave = new GuildMemberJoinRemoveModel();
-                    leave.sendLeaveMessage(event, "leave_channel_only_box", leaveChannel);
-                }
-            } catch (InsufficientPermissionException e) {
-            }
-        }
-    }
-
-    private class GuildMemberJoinRemoveModel {
-
-        private void sendJoinMessage(GuildMemberJoinEvent eventJoin, String channel, PJoinChannel joinChannel) {
-            String fileName = "join.png";
-            if (channel.equals("join_channel_only_box")) {
-                // @formatter:off
-                eventJoin.getGuild().loadMembers()
-
-                   .onSuccess(members -> eventJoin.getGuild().getTextChannelById(joinChannel.getChannelId())
-                           .sendFile(sendCustomImage(eventJoin.getUser(), members.size()), fileName)
-                           .queue())
-
-                   .onError(e -> eventJoin.getGuild().getTextChannelById(joinChannel.getChannelId())
-                           .sendFile(sendCustomImage(eventJoin.getUser(), eventJoin.getGuild().getMemberCount()), fileName)
-                           .queue());
-                   // @formatter:on
-            }
-
-            else if (channel.equals("join_channel_full")) {
-                String quote = "> ";
-                // @formatter:off
-                   eventJoin.getGuild().loadMembers()
-
-                   .onSuccess(members -> eventJoin.getGuild().getTextChannelById(joinChannel.getChannelId())
-                           .sendMessage(quote + joinChannel.getMessage().replace("[member]", eventJoin.getUser().getName()))
-                           .addFile(sendCustomImage(eventJoin.getUser(), members.size()), fileName)
-                           .queue())
-
-                   .onError(e -> eventJoin.getGuild().getTextChannelById(joinChannel.getChannelId())
-                           .sendMessage(quote + joinChannel.getMessage().replace("[member]", eventJoin.getUser().getName()))
-                           .addFile(sendCustomImage(eventJoin.getUser(), eventJoin.getGuild().getMemberCount()), fileName)
-                           .queue());
-                   // @formatter:on
-            }
-        }
-
-        private void sendLeaveMessage(GuildMemberRemoveEvent eventRemove, String channel, PLeaveChannel leaveChannel) {
-            String fileName = "leave.png";
-            if (channel.equals("leave_channel_only_box")) {
-             // @formatter:off
-                eventRemove.getGuild().loadMembers()
-
-                .onSuccess(members -> eventRemove.getGuild().getTextChannelById(leaveChannel.getChannelId())
-                        .sendFile(sendCustomImage(eventRemove.getUser(), members.size()), fileName)
-                        .queue())
-
-                .onError(e -> eventRemove.getGuild().getTextChannelById(leaveChannel.getChannelId())
-                        .sendFile(sendCustomImage(eventRemove.getUser(), eventRemove.getGuild().getMemberCount()), fileName)
-                        .queue());
-                // @formatter:on
-            }
-
-            else if (channel.equals("leave_channel_full")) {
-                String quote = "> ";
-                // @formatter:off
-                eventRemove.getGuild().loadMembers()
-
-                .onSuccess(members -> eventRemove.getGuild().getTextChannelById(leaveChannel.getChannelId())
-                        .sendMessage(quote + leaveChannel.getMessage().replace("[member]", eventRemove.getUser().getName()))
-                        .addFile(sendCustomImage(eventRemove.getUser(), members.size()), fileName)
-                        .queue())
-
-                .onError(e -> eventRemove.getGuild().getTextChannelById(leaveChannel.getChannelId())
-                        .sendMessage(quote + leaveChannel.getMessage().replace("[member]", eventRemove.getUser().getName()))
-                        .addFile(sendCustomImage(eventRemove.getUser(), eventRemove.getGuild().getMemberCount()), fileName)
-                        .queue());
-                // @formatter:on
-            }
-        }
-
-        private ByteArrayInputStream sendCustomImage(User user, int memberCount) {
-            try (ByteArrayOutputStream os = new ByteArrayOutputStream();) {
-                ImageIO.setUseCache(false);
-                BufferedImage image = ImageIO.read(new File(EFolder.RESOURCES.getName() + EFolder.TEMPLATE.getName() + "welcome.png"));
-
-                // @formatter:off
-                BufferedImage avatar = (user.getAvatarUrl() == null) ? 
-                        ImageIO.read(new File(EFolder.RESOURCES.getName() + EFolder.TEMPLATE.getName() + "default_discord_avatar.png")) : 
-                        ImageIO.read(new URL(user.getAvatarUrl() + "?size=128"));
-                // @formatter:on
-
-                Font font = new Font("Calibri", Font.TRUETYPE_FONT, 24);
-                Graphics2D g = image.createGraphics();
-                FontMetrics metrics = g.getFontMetrics(font);
-
-                g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-                g.drawImage(avatar, (image.getWidth() - avatar.getWidth()) / 2, 27, null);
-                g.drawString(defineAttributedText(font, user.getAsTag(), Color.WHITE).getIterator(), (image.getWidth() - metrics.stringWidth(user.getAsTag())) / 2, 195);
-                g.drawString(defineAttributedText(new Font("Lato", Font.BOLD, 14), "#" + memberCount, Color.WHITE).getIterator(), 10, 225);
-                g.dispose();
-
-                ImageIO.write(image, "png", os);
-                return new ByteArrayInputStream(os.toByteArray());
-            } catch (IOException e) {
-            }
-            return null;
-        }
-
-        private AttributedString defineAttributedText(Font font, String text, Color textColor) {
-            AttributedString attributedText = new AttributedString(text);
-            attributedText.addAttribute(TextAttribute.FONT, font);
-            attributedText.addAttribute(TextAttribute.FOREGROUND, textColor);
-            return attributedText;
-        }
-
     }
 
 }
