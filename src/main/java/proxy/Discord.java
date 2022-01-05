9593b2e6a1405fa07527fa03518072f2a45ccf3d
Ph4ntom01 <b4rcode@pm.me>
2022-01-05 17:49:29 +0100

    Add comments
    Add mvc pattern into "addEventListeners" method

--- src/main/java/proxy/Discord.java
+++ src/main/java/proxy/Discord.java
@@ -5,11 +5,14 @@ import javax.security.auth.login.LoginException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import configuration.file.Config;
 import configuration.file.ConfigFactory;
-import listeners.generics.GuildListener;
-import listeners.generics.MemberListener;
-import listeners.generics.ReadyListener;
+import configuration.file.TOMLConfig;
+import handlers.actions.GuildListener;
+import handlers.actions.GuildModel;
+import handlers.actions.MemberListener;
+import handlers.actions.MemberModel;
+import handlers.actions.MemberView;
+import handlers.actions.OnReady;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -22,37 +25,63 @@ public class Discord {
 
     private static final Logger LOG = LoggerFactory.getLogger(Discord.class);
 
-    private Config conf;
+    private TOMLConfig file;
 
     protected Discord() {
-        conf = ConfigFactory.getConf();
+        file = ConfigFactory.getProxy();
     }
 
     protected void connect() {
-        JDABuilder builder = JDABuilder.createDefault(conf.getString("token.discord"));
+        // Creates a JDABuilder with recommended default settings.
+        // MemberCachePolicy is set to DEFAULT.
+        // ChunkingFilter is set to NONE.
+        // GatewayIntent is set to DEFAULT.
+        // CacheFlag.ACTIVITY and CacheFlag.CLIENT_STATUS are disabled.
+        JDABuilder builder = JDABuilder.createDefault(file.getString("token.discord"));
+
+        // Sets the OnlineStatus that the connection will display.
         builder.setStatus(OnlineStatus.ONLINE);
+        // Sets the Activity for the session.
         builder.setActivity(Activity.playing("@Proxy"));
+
+        // Enables the specified GatewayIntents.
+        // GUILD_MEMBERS as a goal to be informed about update/leave/join events of a guild.
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
-        builder.enableIntents(GatewayIntent.GUILD_VOICE_STATES);
+
+        // Configure the Chunking Filter and the Member Cache Policy.
         configureMemoryUsage(builder);
+
+        // Adds all provided listeners to the list of listeners that will be used
+        // to populate the JDA object.
         addEventListeners(builder);
+
+        // Builds the JDA object and start the login process with the provided token.
         build(builder);
     }
 
     private void configureMemoryUsage(JDABuilder builder) {
+        // Filters which guilds should use member chunking on guild initialization.
+        // To use chunking, the GUILD_MEMBERS intent must be enabled! Otherwise you must use NONE!
         builder.setChunkingFilter(ChunkingFilter.NONE);
+
+        // This will decide whether to cache a member (and its respective user).
+        // All members are cached by default.
+        // If a guild is enabled for chunking, all members will be cached for it.
         builder.setMemberCachePolicy(MemberCachePolicy.VOICE);
     }
 
     private void addEventListeners(JDABuilder builder) {
-        builder.addEventListeners(new ReadyListener(), new GuildListener(), new MemberListener());
+        GuildModel guildModel = new GuildModel();
+        MemberModel memberModel = new MemberModel();
+        MemberView memberView = new MemberView();
+        builder.addEventListeners(new OnReady(), new GuildListener(guildModel), new MemberListener(memberModel, memberView));
     }
 
     private void build(JDABuilder builder) {
         try {
             JDA jda = builder.build().awaitReady();
             // Send stats.
-            BotStats stats = new BotStats(conf, Math.toIntExact(jda.getGuildCache().size()));
+            BotStats stats = new BotStats(file, Math.toIntExact(jda.getGuildCache().size()));
             stats.setDiscordBotListGuildCount();
             stats.setBotsOnDiscordGuildCount();
         } catch (LoginException | NullPointerException e) {
